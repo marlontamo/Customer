b2ac3856d26380f4897e0832294bb120053b6308
marlontamo <marlontamo@.mail.com>
2020-03-25 17:07:38 +0800

    modified README and footer

--- application/views/user/Admin_lte_theme/Admin_lte_footer.php
+++ application/views/user/Admin_lte_theme/Admin_lte_footer.php
@@ -2,6 +2,6 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.4.0
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://github.com/marlontamo?tab=repositories">Marlon Tamo</a>.</strong> All rights
+    <strong>Copyright &copy; 2014-2016 <a href="https://github.com/marlontamo/Customer">Marlon Tamo</a>.</strong> All rights
     reserved.
   </footer>
